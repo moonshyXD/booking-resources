72c841af6e94b676a985e68f7d49daddcf342fc2
moonshy <vladbobkb10@gmail.com>
2026-03-08 20:43:44 +0300

    [refactor] edit UserDB.email param to one for company and more for more company

--- src/backend/iam/repository/user.py
+++ src/backend/iam/repository/user.py
@@ -1,5 +1,13 @@
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+    DateTime,
+    ForeignKey,
+    UniqueConstraint,
+)
 from sqlalchemy.sql import func
 
 
@@ -10,10 +18,19 @@ class UserDB(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
+    company_id = Column(
+        Integer, ForeignKey("companies.id", ondelete="CASCADE"), nullable=False
+    )
     email = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
     first_name = Column(String, nullable=True)
     last_name = Column(String, nullable=True)
+    telegram_username = Column(String, nullable=True)
+    avatar_url = Column(String, nullable=True)
     role = Column(String, default="USER", nullable=False)
     is_active = Column(Boolean, default=True, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    __table_args__ = (
+        UniqueConstraint("company_id", "email", name="unique_company_email"),
+    )

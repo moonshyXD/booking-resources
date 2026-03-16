612560a207e1a65f7b8a8e0141700efdd30aa968
mrsskaa <mariabyrtseva@yandex.ru>
2026-03-16 19:38:21 +0300

    add background

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,11 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
+import { store } from './store/store'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-//import 'bootstrap/dist/css/bootstrap.css'
-import App from './App.tsx'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </Provider>
+        </QueryClientProvider>
+    </StrictMode>,
 )

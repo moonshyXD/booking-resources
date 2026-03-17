ef31b21ff73bad9d99f48aff457872e0f5183812
moonshy <vladbobkb10@gmail.com>
2026-03-18 01:01:53 +0300

    [refactor] rename env

--- src/backend/iam/migrations/env.py
+++ src/backend/iam/migrations/env.py
@@ -27,7 +27,7 @@ import repository.models.company
 
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
+# other values from the config, defined by the needs of .env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
